a18c214540bfc2fa49b971945096b0f3fa23a508
David Bliss <davidebliss@gmail.com>
2024-09-17 12:56:49 -0700

    save 2 dimension vectors as well

--- cluster.py
+++ cluster.py
@@ -29,11 +29,14 @@ vectors = np.asarray(
 	dtype=np.double
 )
 
-## first, reduce to 2 dimensions for original chart and playback (visualize.py)
+
+
+reduced_embeddings = PCA(n_components=2).fit_transform(vectors)
+
+## BEGIN SUPPORT OLD FEATURE (2d graph)
 with genres_path.open() as f:
 	genres = json.load(f)
 
-reduced_embeddings = PCA(n_components=2).fit_transform(vectors)
 kmeans = KMeans(init="k-means++", n_clusters=len(genres), n_init=4).fit(reduced_embeddings)
 
 with open(kmeans_song_load_order_path, 'w') as f:
@@ -41,6 +44,19 @@ with open(kmeans_song_load_order_path, 'w') as f:
 
 sio.dump(kmeans, kmeans_path)
 np.save(reduced_embeddings_path, reduced_embeddings)
+## END SUPPORT OLD FEATURE (2d graph)
+conn.execute("""
+  DROP TABLE IF EXISTS songs_vectors_2;
+  
+  CREATE TABLE IF NOT EXISTS songs_vectors_2 (
+	song_id TEXT NOT NULL REFERENCES song(id),
+	embedding JSON NOT NULL
+);""")
+
+with closing(conn.cursor()) as cursor:
+    with conn:
+      for (id, embedding) in zip(song_ids, reduced_embeddings):
+        cursor.execute("INSERT INTO songs_vectors_2 VALUES (?, ?);", (id, (json.dumps((embedding.tolist())))))
 
 
 ## then, reduce to 6 dimensions for new functionality (tbd)
